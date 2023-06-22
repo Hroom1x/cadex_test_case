d30d28c33fcf27f6e939c72b7faf1aa29cd6b7a2
Mikhail T <mihatjgl@mail.ru>
2023-06-22 17:10:05 +0300

    Remove counter from printCoordsAt

--- src/utility.cpp
+++ src/utility.cpp
@@ -8,10 +8,9 @@ std::string formatPointToStr(Point point)
 
 void printCoordsAt(const double& t, const std::list<std::shared_ptr<ICurve>>& curves, std::ostream& output)
 {
-    std::size_t i = 0;
     std::for_each(curves.begin(), curves.end(), [&i, t, &output](const std::shared_ptr<ICurve>& curve)
     {
-        output << ++i << " point: " << formatPointToStr(curve->getPoint(t));
+        output << "Point: " << formatPointToStr(curve->getPoint(t));
         output << ", derivative: " << formatPointToStr(curve->getDerivativePoint(t)) << std::endl;
     });
 }
