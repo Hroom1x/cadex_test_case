95033de5c87e8fb4f631842cd14c6ee2b2c9adda
Mikhail T <mihatjgl@mail.ru>
2023-06-23 21:51:17 +0300

    Complete required task

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 
 int main() {
-
+    /*
     {
         Circle circle(1);
         assert(circle.getRadius() == 1);
@@ -60,8 +60,28 @@ int main() {
         std::list<std::shared_ptr<Circle>> circles;
         populateWithCircles(curves, circles);
         circles.sort(isLessCirclePtr);
+        printCoordsAt(taskParameter, curves, std::cout);
         std::cout << sumCirclesRadii(circles) << std::endl;
-    }
+    }*/
+
+    // Populate a container of objects of curve types created in random manner with
+    // random parameters.
+    std::list<std::shared_ptr<ICurve>> curves;
+    populateWithRandom(100, curves);
+
+    // Print coordinates of points and derivatives of all curves in the container at t=PI/4
+    printCoordsAt(taskParameter, curves, std::cout);
+
+    // Populate a second container that would contain only circles from the first container.
+    std::list<std::shared_ptr<Circle>> circles;
+    populateWithCircles(curves, circles);
+
+    // Sort the second container in the ascending order of circles’ radii with comparator isLessCirclePtr
+    circles.sort(isLessCirclePtr);
+
+    // Compute and print the total sum of radii of all curves in the second container.
+    std::cout << "The total sum of radii of all curves in the second container: "
+    << sumCirclesRadii(circles) << std::endl;
 
     return EXIT_SUCCESS;
 }
